f866aab2ee2e80ea94e551e6e18cbc5847b58b68
seriksen <sam.eriksen@bristol.ac.uk>
2023-09-12 14:23:35 -0400

    test: versioning

--- src/dask_dirac/__init__.py
+++ src/dask_dirac/__init__.py
@@ -8,7 +8,9 @@ dask-dirac: DIRAC Executor for Dask
 from __future__ import annotations
 
 from ._dask import DiracCluster
+from . import _version
 
-__version__ = "0.1.0"
+
+__version__ = _version.get_versions()['version']
 
 __all__ = ("__version__", "DiracCluster")
